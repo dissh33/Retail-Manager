27b0aaa236ceef58592358866e67de71812aa3d5
dissh33 <dissh33@yandex.ru>
2020-02-16 17:25:52 +0300

    Add WPF MVVM project
    Add blank WPF project with Caliburn Micro for MVVM Framework

---
+++ Retail-Manager/RMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using RMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDesktopUI
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
