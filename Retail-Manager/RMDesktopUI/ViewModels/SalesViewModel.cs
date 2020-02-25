c4f4b13254f9161ccffd5c9c4db76dde8082a66e
dissh33 <dissh33@yandex.ru>
2020-02-25 13:50:18 +0300

    Create Sales Page desing
    Adds SalesView and base SalesViewModel

---
+++ Retail-Manager/RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+        private BindingList<string> _cart;
+        private string _itemQuantity;
+
+        public BindingList<string> Products
+        {
+            get => _products; 
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        public BindingList<string> Cart
+        {
+            get => _cart;
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }  
+
+        public string ItemQuantity
+        {
+            get => _itemQuantity;
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+
+        public string SubTotal
+        {
+            get
+            {
+                // TODO - Replace with calculation
+                return "$0.00";
+            }
+        }
+        public string Tax
+        {
+            get
+            {
+                // TODO - Replace with calculation
+                return "$0.00";
+            }
+        }
+        public string Total
+        {
+            get
+            {
+                // TODO - Replace with calculation
+                return "$0.00";
+            }
+        }
+
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is selected
+                //Make sure there is an item quantity
+
+                return output;
+            }
+        }
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is selected
+                //Make sure there is an item quantity
+
+                return output;
+            }
+        }
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                //Make sure something is in the cart
+
+                return output;
+            }
+        }
+        public void CheckOut()
+        {
+
+        }
+
+    }
+}
