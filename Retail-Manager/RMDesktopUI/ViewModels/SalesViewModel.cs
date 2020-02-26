f8e26d9ca33f8ec66a024cb96582bc79df059c12
dissh33 <dissh33@yandex.ru>
2020-02-27 02:18:02 +0300

    Small Changes in Views

--- Retail-Manager/RMDesktopUI/ViewModels/SalesViewModel.cs
+++ Retail-Manager/RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -12,7 +12,7 @@ namespace RMDesktopUI.ViewModels
     {
         private BindingList<string> _products;
         private BindingList<string> _cart;
-        private string _itemQuantity;
+        private int _itemQuantity;
 
         public BindingList<string> Products
         {
@@ -34,13 +34,13 @@ namespace RMDesktopUI.ViewModels
             }
         }  
 
-        public string ItemQuantity
+        public int ItemQuantity
         {
             get => _itemQuantity;
             set
             {
                 _itemQuantity = value;
-                NotifyOfPropertyChange(() => Products);
+                NotifyOfPropertyChange(() => ItemQuantity);
             }
         }
 
