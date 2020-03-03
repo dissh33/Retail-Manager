f21a8c772fb8cfef7c0850242301639e886efb59
dissh33 <dissh33@yandex.ru>
2020-03-03 13:20:04 +0300

    Add Foreign Key Relationships in BD

--- Retail-Manager/RMData/dbo/Tables/Sale.sql
+++ Retail-Manager/RMData/dbo/Tables/Sale.sql
@@ -4,6 +4,7 @@
     [CashierId] NVARCHAR(128) NOT NULL, 
     [SaleDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
     [SubTotal] MONEY NOT NULL, 
-    [Discount] MONEY NOT NULL, 
-    [Total] MONEY NOT NULL
+    [Discount] MONEY NULL, 
+    [Total] MONEY NOT NULL, 
+    CONSTRAINT [FK_Sale_ToUser] FOREIGN KEY ([CashierId]) REFERENCES [User]([Id])
 )
